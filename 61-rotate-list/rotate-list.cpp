5a35539713e70b221196fef4d947cde61fe29ce6
sakshamgarg1162013-dotcom <sakshamgarg116.2013@gmail.com>
2026-07-18 00:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (64.58%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -13,33 +13,55 @@ private:
     ListNode* reverse(ListNode* curr){
         ListNode* pre = NULL;
         ListNode* nex = NULL;
-        while(curr!=NULL){
-            nex = curr ->next;
+
+        while(curr != NULL){
+            nex = curr->next;
             curr->next = pre;
             pre = curr;
             curr = nex;
         }
+
         return pre;
     }
+
+    int length(ListNode* head){
+        int len = 0;
+
+        while(head != NULL){
+            len++;
+            head = head->next;
+        }
+
+        return len;
+    }
+
 public:
     ListNode* rotateRight(ListNode* head, int k) {
 
-        if(k==0)
-        return head;
-        
-        if(head==NULL || head->next == NULL){
+        if(head == NULL || head->next == NULL || k == 0)
             return head;
-        }
+
+        int len = length(head);
+        k %= len;
+
+        if(k == 0)
+            return head;
 
         ListNode* curr = reverse(head);
+
+        // Make circular
         head->next = curr;
-        for(int i=1;i<k;i++){
+
+        // Move to the kth node
+        for(int i = 1; i < k; i++){
             curr = curr->next;
         }
+
         ListNode* temp = curr->next;
         curr->next = NULL;
+
         head = reverse(temp);
 
-        return head; 
+        return head;
     }
 };
